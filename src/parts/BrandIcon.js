1bfcfe568c73e2f361079c981091b73814b940a9
aessayed <ahmad.aessayed@gmail.com>
2022-09-05 11:07:13 +0300

    commend the header

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -2,18 +2,21 @@
 import React from 'react';
 
 import Button from 'elements/Button';
+import iconImg from 'assets/images/salem-group1.jfif';
 
 export default function BrandIcon() {
   return (
-    <Button
+     <Button
       className=""
       type="link"
       href="/"
     >
-      <p className="text-theme-blue text-4xl">
+      
+     <img className="iconImg" src={iconImg} alt="Build Website"/>
+    {/* <p className="text-theme-blue text-4xl">
         Racx
         <span className="text-theme-purple">studio</span>
-      </p>
+  </p> */}
     </Button>
   );
 }
